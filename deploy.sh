b6fb5a8d4812c45ddfb484c2256705b7cca7d530
Anderson Danilo <contato@andersondanilo.com>
2016-02-19 23:10:35 -0200

    script deploy

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+git checkout master
+
+git branch -D client-production-filtered
+git checkout client-production
+git merge master
+git branch client-production-filtered
+git filter-branch -f --subdirectory-filter client -- client-production-filtered
+
+git checkout master
+git branch -D api-filtered
+git branch api-filtered
+git filter-branch -f --subdirectory-filter api -- api-filtered
+
+git push
+#git push origin gh-pages
+#git push heroku heroku-master:master
